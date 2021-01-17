72dee3d3112db846c55ec7eeade6da798efb17d1
unknown <sekta0106@gmail.com>
2021-01-17 10:34:09 +0530

    Committed by Ekta

--- auto2auto/Webservices.java
+++ auto2auto/Webservices.java
@@ -17,7 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
-public class Webservices {
+public class Webservices 
+{
 	
 	private UserRepository user;
 	private ProductsRepository p;
